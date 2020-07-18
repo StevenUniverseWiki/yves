6965bc25242e3a0c66277c4b141f08ca00ba62be
Nozomi <13172293+TheNozomi@users.noreply.github.com>
2020-07-18 04:25:44 -0300

    Add charts

--- frontend/views/AnalyticsView.svelte
+++ frontend/views/AnalyticsView.svelte
@@ -17,6 +17,11 @@
 			<Chart id="5be1e0cb-70c1-481f-9a7a-0b80d2477ef7" theme={theme} />
 			<br />
 			<Chart id="1ae95d7f-0f87-4fc9-9b33-0dfe2a7fa85a" thene={theme} />
+			<br />
+			<Chart id="4abaf4fc-4551-4015-84d6-9993764ce3dc" theme={theme} />
+			<br />
+			<Chart id="350f22d4-ae63-4cf8-a222-d36df924ff0c" theme={theme} />
+			<br />
 		</center>
 	</section>
 </main>
